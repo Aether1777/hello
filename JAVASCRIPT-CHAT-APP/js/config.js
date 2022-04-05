7d439089580404302668897b9ab239d3bb36ce97
Aether1777 <bkevin39415@gmail.com>
2022-04-05 15:05:22 -0700

    s

--- JAVASCRIPT-CHAT-APP/js/config.js
+++ JAVASCRIPT-CHAT-APP/js/config.js
@@ -22,17 +22,17 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const config = {
-    apiKey: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    authDomain: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    databaseURL: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    projectId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    storageBucket: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    messagingSenderId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    appId: "20679348f4c95ee2",
-    measurementId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    CometChatAppId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    CometChatRegion: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    CometChatAuthKey: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    CometChatAPIKey: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
-    CometChatWidgetId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx
+    apiKey: "AIzaSyDodFkfjSEF9Q9UTCRuCwwIVqgPOH0Az5s",
+    authDomain: "hello-6b52b.firebaseapp.com",
+    databaseURL: "https://hello-6b52b-default-rtdb.firebaseio.com",
+    projectId: "hello-6b52b",
+    storageBucket: "hello-6b52b.appspot.com",
+    messagingSenderId: "399394191789",
+    appId: "1:399394191789:web:ac7c3ee9249d835ea7c924",
+    measurementId: "G-6H64LMK0YZ",
+    CometChatAppId: "20679348f4c95ee2",
+    CometChatRegion: "us",
+    CometChatAuthKey: "476cc58d95826d8e124c78d714b1f3873dba5e2f",
+    CometChatAPIKey: "476cc58d95826d8e124c78d714b1f3873dba5e2f",
+    CometChatWidgetId: "35cb098e-9ba7-4710-91ef-d37af279acde"
   };
