06131d1b19bbadda036e42a9c135017f02cc0d23
Aether1777 <bkevin39415@gmail.com>
2022-04-05 14:28:01 -0700

    s

---
+++ JAVASCRIPT-CHAT-APP/js/config.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDodFkfjSEF9Q9UTCRuCwwIVqgPOH0Az5s",
+  authDomain: "hello-6b52b.firebaseapp.com",
+  databaseURL: "https://hello-6b52b-default-rtdb.firebaseio.com",
+  projectId: "hello-6b52b",
+  storageBucket: "hello-6b52b.appspot.com",
+  messagingSenderId: "399394191789",
+  appId: "1:399394191789:web:ac7c3ee9249d835ea7c924",
+  measurementId: "G-6H64LMK0YZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const config = {
+    apiKey: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    authDomain: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    databaseURL: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    projectId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    storageBucket: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    messagingSenderId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    appId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    measurementId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    CometChatAppId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    CometChatRegion: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    CometChatAuthKey: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    CometChatAPIKey: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+    CometChatWidgetId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx
+  };
